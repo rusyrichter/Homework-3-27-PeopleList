a856ad3bc88490917ad80cd7428701bbde2fbb04
Rusy Richter <rusyrichter@gmail.com>
2023-03-28 23:26:49 -0400

    Add project files.

---
+++ class_3-27/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using class_3_27.Models;
+using Microsoft.AspNetCore.Mvc;
+using PoepleDataList;
+using System.Diagnostics;
+
+namespace class_3_27.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        private string _connectionString = @"Data Source=.\sqlexpress;Initial Catalog=People; Integrated Security=true";
+
+        public IActionResult Index()
+        {
+            PeopleListDB db = new PeopleListDB(_connectionString);
+            PeopleViewModel viewModel = new PeopleViewModel
+            {
+                people = db.SelectAll(),
+            };
+
+
+            return View(viewModel);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(List<People> people)
+        {
+            PeopleListDB db = new PeopleListDB(_connectionString);
+            db.AddMany(people);
+            return Redirect("/Home/Index");
+        }
+    }
+}
---
+++ class_3-27/Models/PeopleViewModel.cs
@@ -0,0 +1,9 @@
+﻿using PoepleDataList;
+
+namespace class_3_27.Models
+{
+    public class PeopleViewModel
+    {
+        public List<People> people = new List<People>();
+    }
+}
